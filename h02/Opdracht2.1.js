ea86475bde7b22c165c0a70c69cf940b180c1002
Stefan <sbaraldo@roc-dev.com>
2020-02-18 16:31:07 +0100

    Hoofdstuk 2 af

---
+++ h02/Opdracht2.1.js
@@ -0,0 +1,4 @@
+document.getElementById("paginatitel").innerText = "Dit is een paginatitel.";
+document.getElementById("subkop").innerText = "Dit is een subkop.";
+document.getElementById("kaneki").src = "Img/kaneki.jpg";
+document.getElementById("tokyo ghoul").src = "Img/Tokyo Ghoul manga.jpg";
---
+++ h02/Opdracht2.2.js
@@ -0,0 +1,16 @@
+document.getElementById("bl1").style.backgroundColor = "lightblue";
+document.getElementById("bl2").style.backgroundColor = "green";
+document.getElementById("bl3").style.backgroundColor = "yellow";
+document.getElementById("bl4").style.backgroundColor = "red";
+
+document.getElementById("bl2").style.position = "absolute";
+document.getElementById("bl2").style.top = "25px";
+document.getElementById("bl2").style.left = "100px";
+
+document.getElementById("bl3").style.position = "absolute";
+document.getElementById("bl3").style.top = "50px";
+document.getElementById("bl3").style.left = "200px";
+
+document.getElementById("bl4").style.position = "absolute";
+document.getElementById("bl4").style.top = "0px";
+document.getElementById("bl4").style.left = "300px";
