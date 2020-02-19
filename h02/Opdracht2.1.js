f2defd2b7d74fb8c163baea96a5b4ddc909141e8
Stefan <sbaraldo@roc-dev.com>
2020-02-19 20:54:23 +0100

    Opdracht2.1 af

--- h02/Opdracht2.1.js
+++ h02/Opdracht2.1.js
@@ -1,4 +1,4 @@
 document.getElementById("paginatitel").innerText = "Dit is een paginatitel.";
 document.getElementById("subkop").innerText = "Dit is een subkop.";
-document.getElementById("kaneki").src = "Img/kaneki.jpg";
-document.getElementById("tokyo ghoul").src = "Img/Tokyo Ghoul manga.jpg";
+document.getElementById("kaneki").src = "Img/ghoul6";
+document.getElementById("tokyo ghoul").src = "Img/ghoul7";
